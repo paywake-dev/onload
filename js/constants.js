04981960e866c13594dc4a74c6ea8aa62d6a8096
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-09-19 18:35:42 -0700

    adding /hidebanner page

--- js/constants.js
+++ js/constants.js
@@ -1,2 +1,3 @@
 const YEAR = (new Date()).getFullYear()
 const REDIRECT = "https://paywake.net/?source=dev"
+const REDIRECT_HIDEBANNER = "https://paywake.net/?source=dev&hidebanner=true"
--- js/preload.js
+++ js/preload.js
@@ -1,2 +1,5 @@
 console.log("\u00A9 " + YEAR.toString() + " Paywake Corporation")
+if (window.location.pathname === "/hidebanner") {
+  window.location.replace(REDIRECT_HIDEBANNER);
+}
 window.location.replace(REDIRECT);
