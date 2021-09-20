1f0fdbfc286ebc52547cccdf78f48e651ebef1fe
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-09-19 18:37:42 -0700

    Update preload.js

--- js/preload.js
+++ js/preload.js
@@ -2,4 +2,6 @@ console.log("\u00A9 " + YEAR.toString() + " Paywake Corporation")
 if (window.location.pathname === "/hidebanner") {
   window.location.replace(REDIRECT_HIDEBANNER);
 }
-window.location.replace(REDIRECT);
+else {
+  window.location.replace(REDIRECT);
+}
