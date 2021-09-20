483d823e03a34c0a4c1f1adb4dc3cbba56049ef3
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-09-19 18:22:30 -0700

    catching redirects in dev.js

--- package/dev.js
+++ package/dev.js
@@ -3,9 +3,9 @@
   //TOGGLE THIS ON LAUNCH
   const IS_REDIRECTING = true
 
-  //REDIRECTS
   const REDIRECT = "https://launch.paywake.net"
   const QR_REDIRECT = "https://launch.paywake.net/?source=qr"
+  const LOCAL_STORAGE_TAG = "__paywake-dev"
 
   const SOURCE = (new URLSearchParams(window.location.search)).get("source")
   if (SOURCE === "qr") {
@@ -14,11 +14,19 @@
     }
   }
   else if (SOURCE !== "dev") {
+    localStorage.setItem(LOCAL_STORAGE_TAG, false)
     if (IS_REDIRECTING) {
       window.location.replace(REDIRECT)
     }
   }
   else {
+    localStorage.setItem(LOCAL_STORAGE_TAG, true)
+    const elements = document.body.querySelectorAll("*")
+    for (let element of elements) {
+        if (element.href) {
+            element.href = (element.href + "?source=dev")
+        }
+    }
     console.log("%cDEV", "color: red", "mode enabled")
     if (!(new URLSearchParams(window.location.search)).get("hidebanner")) {
       let banner = document.createElement("div")
