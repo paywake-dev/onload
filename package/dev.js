5bbd29c2892e3b1f3b357d9ed873f19522d7f2fb
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-12-05 16:37:08 -0800

    removing banner

--- package/dev.js
+++ package/dev.js
@@ -21,42 +21,4 @@
       window.location.replace(REDIRECT)
     }
   }
-  else {
-    localStorage.setItem(LOCAL_STORAGE_TAG, true)
-    console.log("%cDEV", "color: red", "mode enabled")
-    if (!(new URLSearchParams(window.location.search)).get("hidebanner")) {
-      let banner = document.createElement("div")
-      banner.className = "__dev-banner"
-      let text = document.createElement("p")
-      text.innerHTML = "You are currently in <B>DEV</B> mode."
-      let close = document.createElement("img")
-      close.src = "https://onload.paywake.net/package/dev.png"
-      close.onclick = () => {
-        banner.remove()
-        window.location.replace(location.href.toString() + "&hidebanner=true")
-      }
-      banner.appendChild(text)
-      banner.appendChild(close)
-      $(window).on("load", () => {
-        const elements = document.body.querySelectorAll("*")
-        for (let e of elements) {
-          if (e.href) {
-            e.href = (e.href + "?source=dev")
-          }
-        }
-        document.body.appendChild(banner)
-      })
-    }
-    else {
-      $(window).on("load", () => {
-        const elements = document.body.querySelectorAll("*")
-        for (let e of elements) {
-          if (e.href) {
-            e.href = (e.href + "?source=dev&hidebanner=true")
-          }
-        }
-      })
-      console.log("%cHIDE-BANNER", "color: lime", "mode enabled")
-    }
-  }
 })()
