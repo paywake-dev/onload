fd21ac1fc93c295b3af1635827404ad496952b96
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-12-05 16:23:38 -0800

    Update dev.js

--- package/dev.js
+++ package/dev.js
@@ -6,6 +6,7 @@
   const REDIRECT = "https://launch.paywake.net"
   const QR_REDIRECT = "https://launch.paywake.net/?source=qr"
   const LOCAL_STORAGE_TAG = "__paywake-dev"
+  const SUBDOMAIN = window.location.host.split(".")[0].toLowerCase()
 
   const SOURCE = (new URLSearchParams(window.location.search)).get("source")
   if (SOURCE === "qr") {
@@ -14,7 +15,7 @@
       window.location.replace(QR_REDIRECT)
     }
   }
-  else if (SOURCE !== "dev") {
+  else if ((SOURCE !== "dev") && (SUBDOMAIN !== "dev")) {
     localStorage.setItem(LOCAL_STORAGE_TAG, false)
     if (IS_REDIRECTING) {
       window.location.replace(REDIRECT)
