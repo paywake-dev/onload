f79d39c417cdb09f3735ed453cca735468f9e5fd
Jack Mandelkorn <jack@jackmandelkorn.com>
2022-02-14 16:31:08 -0800

    Update dev.js

--- package/dev.js
+++ package/dev.js
@@ -21,4 +21,25 @@
       window.location.replace(REDIRECT)
     }
   }
+  else {
+    localStorage.setItem(LOCAL_STORAGE_TAG, true)
+    console.log("%cDEV", "color: red", "mode enabled")
+    if (!(new URLSearchParams(window.location.search)).get("hidebanner")) {
+      let banner = document.createElement("div")
+      banner.className = "__dev-banner"
+      let text = document.createElement("p")
+      text.innerHTML = "You are currently in <B>DEV</B> mode."
+      let close = document.createElement("img")
+      close.src = "https://onload.paywake.net/package/dev.png"
+      close.onclick = () => {
+        banner.remove()
+        window.location.replace(location.href.toString() + "&hidebanner=true")
+      }
+      banner.appendChild(text)
+      banner.appendChild(close)
+    }
+    else {
+      console.log("%cHIDE-BANNER", "color: lime", "mode enabled")
+    }
+  }
 })()
